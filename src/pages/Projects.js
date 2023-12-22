f6e8a6bff2e8dda0ef984606dac8cf02a8d68513
falakthkr <falak@hotmail.co.in>
2023-12-22 10:59:21 +0530

    added projects table

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { Table } from "antd";
+import projectsData from "../data/projects.json";
 
 const Projects = () => {
-  return <h2>Projects</h2>;
+  /** constants */
+  const columns = [
+    {
+      title: "Project ID",
+      dataIndex: "id",
+      sorter: (a, b) => a.id - b.id,
+    },
+    {
+      title: "Project Name",
+      dataIndex: "project_name",
+    },
+    {
+      title: "Company",
+      dataIndex: "developer_name",
+    },
+    {
+      title: "Location",
+      dataIndex: "location",
+    },
+  ];
+
+  /** helper functions */
+  const onChange = (pagination, filters, sorter, extra) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
+
+  /** render functions */
+  const RenderProjectsTable = () => {
+    return (
+      <Table columns={columns} dataSource={projectsData} onChange={onChange} />
+    );
+  };
+
+  return <div className="ProjectsWrapper">{RenderProjectsTable()}</div>;
 };
 
 export default Projects;
