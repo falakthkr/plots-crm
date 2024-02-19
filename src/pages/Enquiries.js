78861f5559693ab2c3bfd72f5f2f28504601d98b
Falak <falak-wareiq@Falaks-MacBook-Pro.local>
2024-02-20 03:23:27 +0530

    added enquiries table as per user flow

--- src/pages/Enquiries.js
+++ src/pages/Enquiries.js
@@ -1,5 +1,99 @@
+import { EditOutlined, EyeOutlined } from "@ant-design/icons";
+import { Space, Table, Tag } from "antd";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Enquiries = () => {
-  return <h1>Enquiries</h1>;
+  const [isLoading, setIsLoading] = useState(true);
+  const navigateTo = useNavigate();
+
+  const renderActions = (data) => {
+    return (
+      <Space size="middle">
+        <EyeOutlined style={{ color: "green" }} />
+        <EditOutlined style={{ color: "blue" }} />
+      </Space>
+    );
+  };
+
+  const columns = [
+    {
+      title: "Plot ID",
+      dataIndex: "plotId",
+      key: "plotId",
+      align: "center",
+    },
+    {
+      title: "Name",
+      dataIndex: "userName",
+      key: "userName",
+      align: "center",
+    },
+    {
+      title: "Ph No.",
+      dataIndex: "userNumber",
+      key: "userNumber",
+      align: "center",
+    },
+    {
+      title: "Status",
+      dataIndex: "userStatus",
+      key: "userStatus",
+      align: "center",
+    },
+    {
+      title: "Actions",
+      render: renderActions,
+      align: "center",
+    },
+  ];
+
+  const data = [
+    {
+      key: 1,
+      plotId: "001",
+      userName: "Samuel Ortega",
+      userNumber: 1233457788,
+      userStatus: "Phase 1",
+    },
+    {
+      key: 2,
+      plotId: "002",
+      userName: "Samuel Ortega",
+      userNumber: 1233457788,
+      userStatus: "Phase 2",
+    },
+    {
+      key: 3,
+      plotId: "003",
+      userName: "Samuel Ortega",
+      userNumber: 1233457788,
+      userStatus: "Phase 1",
+    },
+    {
+      key: 4,
+      plotId: "111",
+      userName: "Samuel Ortega",
+      userNumber: 1233457788,
+      userStatus: "Phase 1",
+    },
+    {
+      key: 5,
+      plotId: "123",
+      userName: "Samuel Ortega",
+      userNumber: 1233457788,
+      userStatus: "Phase 6",
+    },
+    {
+      key: 6,
+      plotId: "088",
+      userName: "Samuel Ortega",
+      userNumber: 1233457788,
+      userStatus: "Phase 2",
+    },
+  ];
+
+  return <Table columns={columns} dataSource={data} />;
 };
 
 export default Enquiries;
